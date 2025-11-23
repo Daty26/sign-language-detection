cc57768ff37883b89b002a6287a1a0309689dae6
Tima <temirlanaltaev26092004@gmail.com>
2025-11-23 19:57:51 +0100

    updated unified classifier

--- classifier/gesture_classifier.py
+++ classifier/gesture_classifier.py
@@ -1,13 +1,23 @@
 from classifier.rule_based import RuleBasedClassifier
+from classifier.ml_classifier import MLClassifier
 
 class GestureClassifier:
     def __init__(self):
-        self.rule_classifier = RuleBasedClassifier()
+        self.rule = RuleBasedClassifier()
+        self.ml = MLClassifier()
 
-    def classify(self, features):
-        result = self.rule_classifier.classify(features)
+        try:
+            self.ml.load()
+            self.use_ml = True
+        except:
+            self.use_ml = False
 
-        if result is None:
-            return "Unknown"
+    def classify(self, features, ml_vector=None):
+        result = self.rule.classify(features)
+        if result != "Unknown":
+            return result
 
-        return result
+        if self.use_ml and ml_vector is not None:
+            return self.ml.predict(ml_vector)
+
+        return "Unknown"
